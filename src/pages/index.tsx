870da8e997a808c07fc51a25c37e6337de7724f7
voigtito <voigt.gustavoh@gmail.com>
2021-12-06 14:55:23 -0300

    Finished challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,36 @@ import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
-import { CardList } from '../components/CardList';
+import { Card, CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+import { AxiosResponse } from 'axios';
+
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type ResponseInterface = {
+  data: Image[];
+  after: string;
+}
 
 export default function Home(): JSX.Element {
+
+  const getImages = async ({ pageParam = null }): Promise<ResponseInterface> => {
+    const response = await api.get('/api/images', {
+      params: {
+        after: pageParam
+      }
+    })
+    return response.data
+  }
+
   const {
     data,
     isLoading,
@@ -18,18 +42,17 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    getImages,
+    { getNextPageParam: (lastPage, pages) => lastPage.after }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(pageData => pageData.data).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
@@ -37,7 +60,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt={8} onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
